b3dd6659080552318f2eb1c382b3845e496c2bda
Jango4936 <89480924+Jango4936@users.noreply.github.com>
2024-10-10 01:10:03 -0500

    Update Main.java

--- soc/Main.java
+++ soc/Main.java
@@ -5,16 +5,15 @@ import java.util.*;
 public class Main {
 	private static Scanner scanner = new Scanner(System.in);
 	private static Map<String, Account> accounts = new HashMap<>();
-	protected static Account currentAccount = null;
+	private static Account currentAccount = null;
 	private static List<Post> postList = new ArrayList<>();
+	private static String input = "";
+	private static boolean isRunning = true;
 
 	public static void main(String[] args) {
-		
-		accounts.put("admin", new Account("admin","admin"));
-		
-		String input = "";
 
-		boolean isRunning = true;
+		accounts.put("admin", new Account("admin", "admin"));
+
 		while (isRunning == true) {
 			Views.mainWindow();
 
@@ -59,7 +58,7 @@ public class Main {
 //       	Views.displayTextArtOptions(currentAccount);
 	}
 
-	public static void register() {
+	private static void register() {
 		Views.accountRegistrationWindow();
 		System.out.print("Enter UserName: ");
 		String userName = scanner.nextLine();
@@ -69,16 +68,16 @@ public class Main {
 		String phoneNumber = scanner.nextLine();
 
 		if (accounts.containsKey(userName) == true) {
-			System.out.println("The username "+ userName +" is already taken!! Press any key to return!!");
+			System.out.println("The username " + userName + " is already taken!! Press any key to return!!");
 		} else {
 			accounts.put(userName, new Account(userName, password, phoneNumber));
-			System.out.println("Account created successfully!! Press any key to return!!");
+			System.out.println("Account created successfully!!\nPress any key to return...");
 		}
 		scanner.nextLine();
 		Views.mainWindow();
 	}
 
-	public static void login() {
+	private static void login() {
 		Views.accountLoginWindow();
 		while (true) {
 			System.out.print("Enter UserName: ");
@@ -87,13 +86,15 @@ public class Main {
 			String password = scanner.nextLine();
 
 			if (accounts.containsKey(userName) == true && accounts.get(userName).getUserPassword().equals(password)) {
-				System.out.println("Account login successfully!! Press any key to continue!!");
-				Account currentAccount = accounts.get(userName);
+				System.out.println(
+						"Account login successfully!! Welcome " + userName + "!!" + "\nPress any key to continue...");
+				currentAccount = accounts.get(userName);
 				scanner.nextLine();
 				viewPost();
 				break;
 			} else {
-				System.out.println("Invalid username or password. Please try again bruh!! Press any key to continue!!");
+				System.out
+						.println("Invalid username or password. Please try again bruh!!\nPress any key to continue...");
 				scanner.nextLine();
 				Views.mainWindow();
 				break;
@@ -102,7 +103,113 @@ public class Main {
 
 	}
 
-	public static void viewPost() {
+	private static void viewPost() {
 		Views.postViewWindow(postList, currentAccount);
+		while (true) {
+			do {
+				System.out.print("Choose: ");
+				input = scanner.nextLine();
+				if (!input.isEmpty() && input.length() == 1) {
+					break;
+				} else if (input.isBlank()) {
+					System.out.println("Invalid input, please don't leave it blank");
+				} else if (input.length() > 1) {
+					System.out.println("Invalid input, please enter only 1 character!!");
+				}
+
+			} while (input.isBlank() || input.length() > 1);
+
+			char option = input.charAt(0);
+			switch (Character.toLowerCase(option)) {
+			case '+':
+				newPost();
+				break;
+			case 'l':
+				currentAccount = null;
+				Views.mainWindow();
+				break;
+			case 'q':
+				isRunning = false;
+				break;
+			default:
+				System.out.println("Invalid input. Please enter '+', 'L', or 'Q'");
+			}
+			if (isRunning == false || currentAccount == null) {
+				break;
+			}
+		}
+
+	}
+
+	private static void newPost() {
+		Views.newPostWindow(currentAccount);
+		while (true) {
+			do {
+				System.out.print("Choose: ");
+				input = scanner.nextLine();
+				if (!input.isEmpty() && input.length() == 1) {
+					break;
+				} else if (input.isBlank()) {
+					System.out.println("Invalid input, please don't leave it blank");
+				} else if (input.length() > 1) {
+					System.out.println("Invalid input, please enter only 1 character!!");
+				}
+
+			} while (input.isBlank() || input.length() > 1);
+
+			char option = input.charAt(0);
+			switch (Character.toLowerCase(option)) {
+			case 't':
+				postText();
+				break;
+			case 'a':
+				postTextArt();
+				break;
+			default:
+				System.out.println("Invalid input. Please enter 'T' or 'A'");
+			}
+
+		}
+	}
+
+	private static void postTextArt() {
+		Views.displayTextArtOptions(currentAccount);
+		while (true) {
+			do {
+				System.out.print("Choose: ");
+				input = scanner.nextLine();
+				if (!input.isEmpty() && input.length() == 1) {
+					break;
+				} else if (input.isBlank()) {
+					System.out.println("Invalid input, please don't leave it blank");
+				} else if (input.length() > 1) {
+					System.out.println("Invalid input, please enter only 1 character!!");
+				}
+
+			} while (input.isBlank() || input.length() > 1);
+
+			char option = input.charAt(0);
+			switch (Character.toLowerCase(option)) {
+			case '0':
+				
+				break;
+			case '1':
+
+				break;
+			case '2':
+
+				break;
+			case '3':
+
+				break;
+			default:
+				System.out.println("Invalid input. Please enter a number");
+			}
+		}
+	}
+
+	private static void postText() {
+		// TODO Auto-generated method stub
+
 	}
 }

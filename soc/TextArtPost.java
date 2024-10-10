40c920d6370c696afce924b453b471f79c98a867
Jango4936 <89480924+Jango4936@users.noreply.github.com>
2024-10-10 06:22:45 -0500

    Update TextArtPost.java

--- soc/TextArtPost.java
+++ soc/TextArtPost.java
@@ -4,6 +4,18 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 public class TextArtPost extends Post {
+	protected char choiceOfArt;
+	
+	
+	TextArtPost(Account postAccount, char choiceOfArt) {
+		super(postAccount);
+		this.choiceOfArt = choiceOfArt;
+	}
+	
+	public char getChoiceOfArt() {
+		return choiceOfArt;
+	}
+
 	public final static List<String[]> OPTIONS;
     
 	static { // initialize when the class is loaded
@@ -56,8 +68,21 @@ public class TextArtPost extends Post {
 
 	@Override
 	public String getFormattedContent() {
-		// TODO Auto-generated method stub
-		return null;
+	    StringBuilder formattedText = new StringBuilder();
+	    int index = Character.getNumericValue(choiceOfArt);
+	    
+	    for (int i = 0; i < OPTIONS.get(index).length; i++) {
+	        // Get the current line of art
+	        String artLine = OPTIONS.get(index)[i];
+	        
+	        // Ensure the line is padded to exactly 40 characters (left-aligned)
+	        String paddedLine = String.format("%10s%-30s", "",artLine);
+	        
+	        // Append the formatted line with "|" on both sides
+	        formattedText.append("|" + paddedLine + "|\n");
+	    }
+	    
+	    return formattedText.toString();
 	}
 
 }

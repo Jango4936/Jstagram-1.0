a1e9b7cae094842efaf39d8e15de623875f07e2b
Jango4936 <89480924+Jango4936@users.noreply.github.com>
2024-10-24 04:31:13 -0500

    Update Post.java

--- soc/Post.java
+++ soc/Post.java
@@ -11,6 +11,14 @@ public abstract class Post implements ContentFormatter {
     	this.postAccount = postAccount;
     	this.postTime = LocalDateTime.now();
     }
+    
+    public LocalDateTime postTime () {
+    	return postTime;
+    }
+    
+    public Account getPostAccount () {
+    	return postAccount;
+    }
 
     public String tag() {
         String s = postTime.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
